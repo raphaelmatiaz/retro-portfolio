3262600bdef2d350c202965240528f58133eee57
Rafael Matias <rafamatias102@gmail.com>
2026-03-09 21:18:52 +0000

    improve ProjectCard component Add multiple project cards showcasing personal projects

--- retro-portfolio/src/app/page.tsx
+++ retro-portfolio/src/app/page.tsx
@@ -106,14 +106,82 @@ const techLogos = [
             </section>
             
             <section id="projects" className="w-full min-h-screen bg-[var(--color-blue)] text-white border-[4px] border-[var(--color-gray-medium)] p-8">
-              <h2 className="text-4xl font-bold mb-8 text-center">Projects</h2>
-              {/* <p className="text-lg text-center">Projects section content coming soon...</p> */}
+              <h2 className="text-4xl font-bold mb-8 text-center">My Projects</h2>
+             
               <span>
-                <h3 className="text-2xl font-bold mb-8">Learning Projects</h3>
-              <ProjectCard></ProjectCard>
+                <h3 className="text-2xl font-bold mb-8 text-center">Learning Projects (for fun and learning 😎)</h3>
+                <div className="flex flex-wrap justify-center gap-8">
+                  <ProjectCard
+                    projectSummary="A simple web app that generates absurd memes by combining randomized stock images with randomized english words."
+                    projectName="Absurd Meme Generator"
+                    projectImage="/images/meme-generator.webp"
+                    projectNumber={1}
+                    projectTechnologies={["HTML", "CSS", "JavaScript"]}
+                    projectFeatures={["Procedural meme generation", "«Download Meme» Button"]}
+                    projectCtaText="Generate a fun meme!"
+                    projectCtaLink="https://raphaelmatiaz.github.io/Absurd-Meme-Generator/"
+                    projectGithubLink="https://github.com/raphaelmatiaz/Absurd-Meme-Generator"
+                  ></ProjectCard>
+                  <ProjectCard
+                    projectSummary="A web app that creates minimalist randomized abstract art. Based on the JavaScript math.random method, input settings allow you to explore the output and create unique patterns."
+                    projectName="Abstract Art Generator"
+                    projectImage="/images/art-generator.webp"
+                    projectNumber={1}
+                    projectTechnologies={["HTML", "CSS", "JavaScript"]}
+                    projectFeatures={["Generate Meme Button", "Download Meme Button"]}
+                    projectCtaText="Create some crazy art!"
+                    projectCtaLink="https://raphaelmatiaz.github.io/Abstract-Art-Generator/"
+                    projectGithubLink="https://github.com/raphaelmatiaz/Abstract-Art-Generator"
+                  ></ProjectCard>
+                  <ProjectCard
+                    projectSummary="A narrative focused, choices matter, text-based indie game inspired by the 'Dark Souls' game series. Played entirely inside the terminal. Its the first game I ever created!"
+                    projectName="ETIC Souls"
+                    projectImage="/images/etic-souls.webp"
+                    projectNumber={1}
+                    projectTechnologies={["Python"]}
+                    projectFeatures={["Custom character creation","Logical tree of choices and their consequences", "Ascii art UI"]}
+                    projectCtaText="View Installation Instructions"
+                    projectCtaLink="https://github.com/raphaelmatiaz/ETIC-SOULS"
+                    projectGithubLink="https://github.com/raphaelmatiaz/ETIC-SOULS"
+                  ></ProjectCard>
+                  <ProjectCard
+                    projectSummary="Bombr is a social network platform designed as a simplified Instagram clone with a unique twist: users can only and exclusively post content (bomb!) on their friends' profiles instead of their own."
+                    projectName="Bombr"
+                    projectImage="/images/bombr.webp"
+                    projectNumber={1}
+                    projectTechnologies={["TypeScript", "React", "CSS", "Next.js",]}
+                    projectFeatures={["Login / Register / Logout","Google, Discord and Github OAuth methods", "Homepage Feed", "Post pictures, gifs and text on your friends profiles", "Personal Profile for each user", "Home, User Search, Post, Notifications and profile routes", "Responsive Design (mobile / tablet / desktop)"]}
+                    projectCtaText="Watch a Video Showcase!"
+                    projectCtaLink="https://www.youtube.com/watch?v=b_NQkbWEgDk&t=1s"
+                    projectGithubLink="https://github.com/raphaelmatiaz/Bombr"
+                  ></ProjectCard>
+                </div>
 
-                <h3>Serious Projects</h3>
-              
+                <h3 className="text-2xl font-bold mb-8 text-center mt-64">Serious Projects (oouh Very Serious  🧐)</h3>
+                <div className="flex flex-wrap justify-center gap-8">
+                  <ProjectCard
+                    projectSummary="A technical challenge consisting in the design refactor and its code implementation, for a 2 sided marketplace SAAS platform."
+                    projectName="LicitaNow (interview challenge)"
+                    projectImage="/images/licita-now.webp"
+                    projectNumber={1}
+                    projectTechnologies={["HTML", "CSS", "JavaScript"]}
+                    projectFeatures={["Figma ReDesign", "Responsive Design (mobile, tablet, desktop)"]}
+                    projectCtaText="View my Solution!"
+                    projectCtaLink="https://raphaelmatiaz.github.io/LicitaNowChallenge/"
+                    projectGithubLink="https://github.com/raphaelmatiaz/LicitaNowChallenge"
+                  ></ProjectCard>
+                  <ProjectCard
+                    projectSummary="An indie 3D puzzle platformer game, set in an uncanny laundromat, with unique puzzle gameplay and mechanics, mainly inspired by the 'Portal' game series. With pshichological horror mood and a Nietzchean-Lovecraftian metaphorical narrative, LAUNDROMAT is my very first commercially aimed game, currently in development by solo developer: me! 🙂"
+                    projectName="LAUNDROMAT"
+                    projectImage="/images/laundromat.webp"
+                    projectNumber={1}
+                    projectTechnologies={["C#", "Unity Engine 6"]}
+                    projectFeatures={["3D Player Controller", "Gravity flipper", "Levitation of objects", "Numerous puzzle mechanics (pressure plates, buttons...)","Diverse enviroment hazards to mess with the player", "Original Soundtrack", "Original story", "...and many other things that would fill the whole page if I listed them all..."]}
+                    projectCtaText="Wishlist the game on Steam!"
+                    projectCtaLink="https://store.steampowered.com/app/3936500/Laundromat/"
+                    projectInstagramLink="https://www.instagram.com/laundromat_game/"
+                  ></ProjectCard>
+                </div>
               </span>
             </section>
             
